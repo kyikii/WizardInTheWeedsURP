2dec08ccf463f57e954c365f3b471680f9e121bd
EricCousins <cousieri@sheridancollege.ca>
2023-04-26 21:53:19 -0400

    Phat Menu Update
    You didn't think I would do it did you?
    Well get cheesed I think this is cool.

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,20 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Fungus;
 
 public class MenuScript : MonoBehaviour
 {
+    Animator MenuAnims;
+    [SerializeField]Flowchart ViewCommands;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MenuAnims = GetComponent<Animator>();
+        ReturnMain();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SelectLevel()
+    {
+        MenuAnims.SetBool("Levels",true);
+        MenuAnims.SetBool("Main",false);
+        ViewCommands.ExecuteBlock("Levels");
+    } 
+
+    public void SelectCredits()
     {
         
+        MenuAnims.SetBool("Credits",true);
+        MenuAnims.SetBool("Main",false);
+        ViewCommands.ExecuteBlock("Credits");
     }
+
+    public void ReturnMain()
+    {
+        MenuAnims.SetBool("Main",true);
+        MenuAnims.SetBool("Credits",false);
+        MenuAnims.SetBool("Levels",false);
+        ViewCommands.ExecuteBlock("Main");
+    }
+
+    public void LaunchExit()
+    {
+        ViewCommands.ExecuteBlock("Exit");
+        MenuAnims.SetBool("Main",false);
+    }
+
+    public void Running()
+    {
+        if(MenuAnims.GetBool("Running"))
+        {
+            MenuAnims.SetBool("Running",false);
+        }
+        else
+        {
+            MenuAnims.SetBool("Running",true);
+        }
+    }
+
+
+
+
+
 
     public void LauchDev()
     {
